8365bedd2e6b4f91c22c570ec497dc9350280d7c
raohonghua <raohonghua@foxmail.com>
2019-03-06 21:57:38 +0800

    Initial commit

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,3 +1,14 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const base = require('./webpack.base.config');
+const SWPrecachePlugin = require('sw-precache-webpack-plugin');
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
+
+const config = merge(base, {
+  entry: {
+    app: './src/entry-client.js',
+  },
+  plugins: {
+    
+  }
+})
