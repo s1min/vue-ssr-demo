3b51cf0d6e4f47b5b4b653e9169c81ce3cce3fb0
raohonghua <raohonghua@foxmail.com>
2019-03-12 21:54:10 +0800

    Initial commit

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -8,7 +8,31 @@ const config = merge(base, {
   entry: {
     app: './src/entry-client.js',
   },
-  plugins: {
-    
-  }
-})
+  plugins: [
+    // 从 Vue 中剥离 dev-only 代码
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.VUE_ENV': '"client"',
+    }),
+    // 为了更好的缓存提取第三方模块
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function(module) {
+        // 如果某个模块被提取到第三方块中
+        return (
+          // 它在 node_modules 中时
+          /node_modules/.test(module.context) &&
+          // 并且不是一个 CSS 文件
+          !/\.css$/.test(module.request)
+        );
+      },
+    }),
+    // 提取 webpack runtime 和 manifest 以避免第三方块在每次构建的时候改变
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+    }),
+    new VueSSRClientPlugin(),
+  ],
+});
+
+module.exports = config;
